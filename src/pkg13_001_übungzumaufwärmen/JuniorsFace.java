aa4b9a0a364aaff83c9730b281fcbe7c8faeba58
B201 <B201@B104PC08>
2017-08-23 09:26:17 +0200

    Übung Interfaces - Über-Interface, Vererbung

---
+++ src/pkg13_001_übungzumaufwärmen/JuniorsFace.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg13_001_übungzumaufwärmen;
+
+/**
+ *
+ * @author B201
+ */
+public class JuniorsFace implements Junior{
+
+    @Override
+    public String getName() {
+        
+        return "Juniorsface";
+        
+        
+    
+    }
+
+    @Override
+    public String manipuliere(String string) {
+        
+        return "Ich mach doch gar nix :O";
+    }
+    
+}
